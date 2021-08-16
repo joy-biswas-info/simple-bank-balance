dc4c605560871ce507de9bd79dbf304c3872b0e1
joy biswas <contact.joybiswas@gmail.com>
2021-08-17 00:17:17 +0600

    Functional Bank

--- account.js
+++ account.js
@@ -1,49 +1,48 @@
-document.getElementById("diposit").addEventListener("click", function () {
-  const dipositAmmount = document.getElementById("diposit-ammount");
-  const totalDiposit = document.getElementById("total-diposit");
-  const totalDipositText = totalDiposit.innerText;
-  const dipositAmmountValue = parseFloat(dipositAmmount.value);
-  if (dipositAmmountValue < 0 || dipositAmmount.value.length == 0) {
-    alert("Please input A Valid Ammount");
+function convertInput(fieldId) {
+  const inputField = document.getElementById(fieldId);
+  const inputFieldText = inputField.value;
+  const inputFieldAmmount = parseFloat(inputFieldText);
+  inputField.value = "";
+  return inputFieldAmmount;
+}
+
+function currentBalance() {
+  const getStr = document.getElementById("total-balance");
+  const getStrText = getStr.innerText;
+  const getStrValue = parseFloat(getStrText);
+  // getStr.innerText = dipositAmmount;
+  return getStrValue;
+}
+
+function updateTotalField(totalFieldId, amount) {
+  const total = document.getElementById(totalFieldId);
+  const totalText = total.innerText;
+  const totalAmmount = parseFloat(totalText);
+  total.innerText = totalAmmount + amount;
+}
+
+function updateBalance(ammount, isAdd) {
+  const balance = document.getElementById("total-balance");
+  const previousBalance = currentBalance();
+  if (isAdd == true) {
+    balance.innerText = previousBalance + ammount;
   } else {
-    // Update Diposit Ammount
-    const totalDipositAmmount = parseFloat(totalDipositText);
-    const updateDipositAmmount =
-      parseFloat(dipositAmmount.value) + totalDipositAmmount;
-    totalDiposit.innerText = updateDipositAmmount;
-
-    // Update Balance Ammount
-    const totalBalance = document.getElementById("total-balance");
-    const totalBalanceAmmount = parseFloat(totalBalance.innerText);
-    const updatedTotalBalance =
-      totalBalanceAmmount + parseFloat(dipositAmmount.value);
-    totalBalance.innerText = updatedTotalBalance;
+    balance.innerText = previousBalance - ammount;
   }
+}
 
-  dipositAmmount.value = " ";
+document.getElementById("diposit").addEventListener("click", function () {
+  const dipositAmmount = convertInput("diposit-ammount");
+  if (dipositAmmount > 0) {
+    updateTotalField("total-diposit", dipositAmmount);
+    updateBalance(dipositAmmount, true);
+  }
 });
 
 document.getElementById("widthdraw").addEventListener("click", function () {
-  // Get All Element Of widthdraw
-  const totalWidthdraw = document.getElementById("total-widthdraw");
-  const totalWidthdrawText = totalWidthdraw.innerText;
-  const totalWidthdrawAmmount = parseFloat(totalWidthdrawText);
-  const widthdraw = document.getElementById("widthdraw-ammount");
-  const widthdrawText = widthdraw.value;
-  const widthdrawAmmount = parseFloat(widthdrawText);
-
-  // Get All Elements Of Balance
-  const balance = document.getElementById("total-balance");
-  const balanceText = balance.innerText;
-  const balanceAmmount = parseFloat(balanceText);
-  if (widthdrawAmmount < 0 || widthdrawText.length == 0) {
-    alert("Please Input A Valid Ammount To widthdraw");
-  } else if (balanceAmmount <= 0 || widthdrawAmmount > balanceAmmount) {
-    alert("You Have No Sufficent balance");
-  } else {
-    totalWidthdraw.innerText = totalWidthdrawAmmount + widthdrawAmmount;
-    balance.innerText = balanceAmmount - widthdrawAmmount;
+  const widthdrawAmmount = convertInput("widthdraw-ammount");
+  if (widthdrawAmmount > 0 && widthdrawAmmount < currentBalance()) {
+    updateTotalField("total-widthdraw", widthdrawAmmount);
+    updateBalance(widthdrawAmmount, false);
   }
-
-  widthdraw.value = "";
 });
